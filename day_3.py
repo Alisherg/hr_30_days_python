bcac93fe355ebb31dc009354845a7eb14fd0cf90
Alisher Ganiev <alisher_ganiev@apple.com>
2020-11-23 20:59:05 -0800

    Day 3

---
+++ day_3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def main():
+
+    n = int(input())
+
+    if n % 2 != 0:
+        print("Weird")
+    elif (n % 2 == 0) and (2 <= n <= 5):
+        print("Not Weird")
+    elif (n % 2 == 0) and (6 <= n <= 20):
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+if __name__ == "__main__":
+    main()
